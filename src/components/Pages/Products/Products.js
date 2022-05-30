b3145538a877e118772cee40fbf035e13ea915c1
ArmenMatevosyan <armen.matevosyan7@gmail.com>
2022-05-30 22:48:35 +0400

    Fixed db.json, Products.js images and css file

--- src/components/Pages/Products/Products.js
+++ src/components/Pages/Products/Products.js
@@ -1,30 +1,19 @@
 import { useProducts } from "../../../contexts/ProductsProvider";
 import classes from "./Products.module.css";
 
-import src from '../../../images/drone.jpg';
-
 const Products = () => {
     const {productsData} = useProducts()
-    // const src = '../../../images/';
     return (
         <main className={classes.container}>
             {
                 productsData.map(product => {
                     return (
                         <div className={classes.product} key={product.id}>
-                            <img src={src} alt={product.name}></img>
-                            {/* cosole.log({`${src}${product.picture}`}) */}
-                            {/* <img src={require(`${src}${product.picture}`)} alt={product.name}></img> */}
-                            {/* <img src={`${src}+${product.picture}`} alt={product.name}></img> */}
-                            {/* <img src={`../../../images/drone.jpg`} alt={product.name}></img> */}
-                            {/* ${product.picture} */}
-                            
-                            {/* <p>Category: {product.category}</p> */}
-                            {/* <p>{product.picture}</p> */}
+                            <img src={product.picture} alt={product.name}></img>
+                            <span>Category: {product.category}</span>
                             <p>{product.name}</p>
                             <span>{product.description}</span>
-                            <p>{product.price}</p>
-                            <button>Add to Card</button>
+                            <p>{product.price} <button>Add to Card</button></p>
                         </div>
                     );
                 })
